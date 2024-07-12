42d8d28b9010e2a83b8238fe589b86f089ae1b12
Sarveshgith <130198294+Sarveshgith@users.noreply.github.com>
2024-07-12 21:46:18 +0530

    Minimal StudentRoute.js

--- Routes/StudentRoute.js
+++ Routes/StudentRoute.js
@@ -2,11 +2,10 @@ const express = require("express");
 const router = express.Router();
 const ValidToken = require("../Middleware/Auth");
 
-const { RegisterUser, LoginUser, GetBooks, GetBook } = require("../Controllers/StudentController");
+const { RegisterUser, LoginUser, GetBook } = require("../Controllers/StudentController");
 
 router.post("/register", RegisterUser);
 router.post("/login", LoginUser);
-router.get("/books", ValidToken, GetBooks);
-router.get("/books/search", ValidToken, GetBook);
+router.get("/books", ValidToken, GetBook);
 
 module.exports = router;
