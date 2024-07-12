cb5328fdbc53951db21c36d8769e6317b92a3e09
Sarveshgith <130198294+Sarveshgith@users.noreply.github.com>
2024-07-12 20:29:02 +0530

    StudentRoute.js

--- Routes/StudentRoute.js
+++ Routes/StudentRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { RegisterUser, LoginUser, GetBooks, GetBook } = require("../Controllers/StudentController");
 const router = express.Router();
 const ValidToken = require("../Middleware/Auth");
+
+const { RegisterUser, LoginUser, GetBooks, GetBook } = require("../Controllers/StudentController");
 
 router.post("/register", RegisterUser);
 router.post("/login", LoginUser);
 router.get("/books", ValidToken, GetBooks);
 router.get("/books/search", ValidToken, GetBook);
-//api/student/books/search?author=Colleen%20Hoover
+
 module.exports = router;
